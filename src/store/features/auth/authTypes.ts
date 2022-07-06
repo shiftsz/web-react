8031c7f8030d1e4286a92a8af09ab80d609cf9b2
Ilia Kamilov <iliakmlv@gmail.com>
2022-07-06 19:40:27 +0300

    auth types

---
+++ src/store/features/auth/authTypes.ts
@@ -0,0 +1,51 @@
+import { SerializedError } from "@reduxjs/toolkit"
+
+namespace AuthTypes {
+    export enum Status {
+        IDLE = 'idle',
+        PENDING = 'pending',
+        FULFILLED = 'fulfilled',
+        REJECTED = 'rejected'
+    }
+
+    export type Role = {
+        level: number
+        value: string
+        label: string
+    }
+
+    export type User = {
+        id: string
+        forename: string
+        surname: string
+        role: Role
+    }
+
+    export type Payload = {
+        user: User
+        token: string
+    }
+
+    export type IdleState = {
+        status: Status.IDLE
+    }
+
+    export type PendingState = {
+        status: Status.PENDING
+    }
+
+    export type FulFilledState = {
+        status: Status.FULFILLED,
+        payload: Payload
+    }
+
+    export type RejectedState = {
+        status: Status.REJECTED,
+        reason: string
+        error: SerializedError
+    }
+
+    export type State = IdleState | PendingState | FulFilledState | RejectedState
+}
+
+export default AuthTypes
